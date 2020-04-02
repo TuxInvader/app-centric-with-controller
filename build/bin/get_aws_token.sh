eeed87948523b85baf6b6b7b822ab4630a3fe68e
Mark Boddington <mark.boddington@nginx.com>
2020-04-02 10:25:29 +0100

    update secrets from script

--- build/bin/get_aws_token.sh
+++ build/bin/get_aws_token.sh
@@ -4,11 +4,43 @@ account=$(grep aws_account_id secrets/aws_config.yaml | awk '{ print $2 }')
 role=$(grep aws_user_role secrets/aws_config.yaml | awk '{ print $2 }')
 token=$(grep accessToken ~/.aws/sso/cache/* | sed -e 's/.*accessToken": "\([^"]*\).*/\1/')
 
-read -p "Execute login first? (y|n)? " login
-if [ "$login" == "y" ]
+output=$( aws sso get-role-credentials --account-id "$account" --role-name "$role" --access-token "$token" )
+rcode=$?
+
+if [ "$rcode" != "0" ]
 then
+  echo stderr ""
+  echo stderr "*************************************************"
+  echo stderr "Failed to get credentials. Refreshing your login."
+  echo stderr "*************************************************"
+  echo stderr ""
   aws sso login
+  sleep 2
+  token=$(grep accessToken ~/.aws/sso/cache/* | sed -e 's/.*accessToken": "\([^"]*\).*/\1/')
+  output=$( aws sso get-role-credentials --account-id "$account" --role-name "$role" --access-token "$token" )
+  rcode=$?
 fi
 
-aws sso get-role-credentials --account-id "$account" --role-name "$role" --access-token "$token"
+if [ "$rcode" != "0" ]
+then
+  echo stderr ""
+  echo stderr "*************************************************"
+  echo stderr "Failed Again. Giving Up. Sorry it didn't work out."
+  echo stderr "*************************************************"
+  echo stderr ""
+  exit $rcode
+fi
+
+access_key=$( echo "$output" | sed -ne 's/.*accessKeyId: \([^ ]*\).*/\1/p' )
+secret_key=$( echo "$output" | sed -ne 's/.*secretAccessKey: \([^ ]*\).*/\1/p' )
+session_key=$( echo "$output" | sed -ne 's/.*sessionToken: \([^ ]*\).*/\1/p' )
+
+echo "Access_key: $access_key"
+echo "Secret_key: $secret_key"
+echo "Session:    $session_key"
+
+# Update tokens
+sed -ie "s/aws_access_key: .*/aws_access_key: $access_key/" secrets/aws_config.yaml
+sed -ie "s/aws_secret_key: .*/aws_secret_key: $secret_key/" secrets/aws_config.yaml
+sed -ie "s/aws_sts_token: .*/aws_sts_token: $session_key/" secrets/aws_config.yaml
 
